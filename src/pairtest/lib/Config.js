f08299d55a264017acc6e361009972731027d9af
John1968 <john.ainsworth.68@outlook.com>
2025-09-14 20:55:12 +0100

    chore: add config to define business assumptions

---
+++ src/pairtest/lib/Config.js
@@ -0,0 +1,14 @@
+export const BASE_RESERVATION_OBJECT = {
+    ADULT: 0,
+    CHILD: 0,
+    INFANT: 0,
+};
+
+export const TICKET_BOOKING_CONSTANTS = {
+    ADULT: {type: 'ADULT', cost: 25},
+    CHILD: {type: 'CHILD', cost: 15},
+    INFANT: {type: 'INFANT', cost: 0},
+};
+
+export const MAX_TICKETS = 25;
+export const MIN_TICKETS = 1;
